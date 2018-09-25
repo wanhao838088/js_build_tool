2fba13d4760ef555f45762708557921afcf9bd98
llh <838088516@qq.com>
2018-09-25 21:56:50 +0800

    webpack配置

---
+++ webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path'); //path内置的模块，用来设置路径。
+
+module.exports = {
+    entry: './src/js/entry.js',   // 入口文件
+    output: {                     // 输出配置
+        filename: 'bundle.js',      // 输出文件名
+        path: path.resolve(__dirname, 'dist')   //输出文件路径配置
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+
+};
